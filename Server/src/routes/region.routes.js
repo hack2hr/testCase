96f8c98286580075fd9ccedd87b5f7fa56e3097e
Алексей Жуков <gkvalex7@gmail.com>
2020-11-27 00:12:28 +0300

    fix tables

--- Server/src/routes/region.routes.js
+++ Server/src/routes/region.routes.js
@@ -7,10 +7,10 @@ const router = Router();
 // /api/region/getAll
 router.get(
     '/getAll',
-    wrapAccess(auth, access.region.getAll),
+   //  wrapAccess(auth, access.region.getAll),
     wrapResponse(async (request, response) => {
        const regions = await request.client.query(
-          db.queries.getByFields('region')
+          db.queries.getByFields('regions')
        ).then(db.getAll).catch((e) => handleDefault(response, e));
  
        response.json({ regions: regions });
